2a31b60ff4ba96c6c8eb4872d36009f6e746f086
Nicolas AYOUB <nicolas.ayoub5@outlook.com>
2021-04-26 14:49:04 +0200

    mise a jour nombreLong a 4 apres ajout variable positionParent dans classe Stagiaire

--- src/fr/eql/ai109/projet1/Parametre.java
+++ src/fr/eql/ai109/projet1/Parametre.java
@@ -6,13 +6,14 @@ public interface Parametre {
 	final String cheminFichier = System.getProperty("user.dir") + "\\stagiaires.txt";
 	final String cheminFichierCible = System.getProperty("user.dir") + "\\stagiaires.bin";
 	final String cheminFichierTrieOrdreCroissant = System.getProperty("user.dir") + "\\stagiairesEnOrdre.txt";
+	final File fichierInitial = new File(cheminFichier);
 	final File fichierCible = new File(cheminFichierCible);
 	final File fichierTrieOrdreCroissant = new File(cheminFichierTrieOrdreCroissant);
 	final int nombreChamp = 5;
 	final int tailleLong = 8;
-	final int nombreLong = 3;
+	final int nombreLong = 4;
 	final int tailleChampMax = 30;
-	final int tailleStagiaire = (tailleChampMax * nombreChamp) + (tailleLong * 3);
+	final int tailleStagiaire = (tailleChampMax * nombreChamp) + (tailleLong * nombreLong);
 	final long nombreStagiaire = fichierCible.length() / tailleStagiaire;
-
+	
 }
