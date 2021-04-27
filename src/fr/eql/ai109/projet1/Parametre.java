4476251ae03cfcd9c23c274f31f84d47046c231d
Nicolas AYOUB <nicolas.ayoub5@outlook.com>
2021-04-27 17:32:54 +0200

    ajout message des erreurs + fichierDoc

--- src/fr/eql/ai109/projet1/Parametre.java
+++ src/fr/eql/ai109/projet1/Parametre.java
@@ -6,14 +6,22 @@ public interface Parametre {
 	final String cheminFichier = System.getProperty("user.dir") + "\\stagiaires.txt";
 	final String cheminFichierCible = System.getProperty("user.dir") + "\\stagiaires.bin";
 	final String cheminFichierTrieOrdreCroissant = System.getProperty("user.dir") + "\\stagiairesEnOrdre.txt";
+	final String cheminFichierDoc = System.getProperty("user.dir") + "\\README.txt";
 	final File fichierInitial = new File(cheminFichier);
 	final File fichierCible = new File(cheminFichierCible);
 	final File fichierTrieOrdreCroissant = new File(cheminFichierTrieOrdreCroissant);
+	final File fichierDoc = new File(cheminFichierDoc);
 	final int nombreChamp = 5;
 	final int tailleLong = 8;
 	final int nombreLong = 4;
 	final int tailleChampMax = 30;
 	final int tailleStagiaire = (tailleChampMax * nombreChamp) + (tailleLong * nombreLong);
 	final long nombreStagiaire = fichierCible.length() / tailleStagiaire;
-	
+	final String messageErreurFormatNumbero = "L'année choisi n'est pas un nombre. Veuillez saisir un nombre.";
+	final String messageErreurAnneQuatreChiffre = "L'année saisi doit être un nombre à quatre chiffres. Veuillez resaisir.";
+	final String messageFormationTailleGrande = "La taille de la formation ne peut pas depasser " + tailleChampMax + " caractères.";
+	final String messageDepTailleGrande = "La taille du departement ne peut pas depasser " + tailleChampMax + " caractères.";
+	final String messagePrenomTailleGrande = "La taille du prénom ne peut pas depasser " + tailleChampMax + " caractères.";
+	final String messageNomTailleGrande = "La taille du nom ne peut pas depasser " + tailleChampMax + " caractères.";
+	final String messageDocumentationErreur = "Imposseible d'ouvrir la documentation.";
 }
